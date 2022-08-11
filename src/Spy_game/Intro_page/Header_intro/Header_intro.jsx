f4214d60501735bc7a0ff550833dfc1b480442b4
Vahe <vahehovakimyan574@gmail.com>
2022-08-11 22:04:17 +0400

    ch

--- src/Spy_game/Intro_page/Header_intro/Header_intro.jsx
+++ src/Spy_game/Intro_page/Header_intro/Header_intro.jsx
@@ -1,6 +1,6 @@
-import Rules from '../Intro_page_icons_and_image/Rules.png';
-import Rate from '../Intro_page_icons_and_image/Rate.png';
-import Languages from '../Intro_page_icons_and_image/Languages.png';
+// import Rules from '../Intro_page_icons_and_image/Rules.png';
+// import Rate from '../Intro_page_icons_and_image/Rate.png';
+// import Languages from '../Intro_page_icons_and_image/Languages.png';
 import './Header_intro.scss';
 import './Header_intro_media.scss';
 
@@ -9,9 +9,9 @@ export default function Header_Intro() {
     return (
         <>
             <div className="Header_intro_main">
-                <div><img src={Rules} alt="Question icon" className='elem'/></div>
+                {/* <div><img src={Rules} alt="Question icon" className='elem'/></div>
                 <div><img src={Rate} alt="Rate icon" className='elem'/></div>
-                <div><img src={Languages} alt="Languages icon" className='elem'/></div>
+                <div><img src={Languages} alt="Languages icon" className='elem'/></div> */}
             </div>
         </>
     )
--- src/Spy_game/Intro_page/Intro_page.jsx
+++ src/Spy_game/Intro_page/Intro_page.jsx
@@ -1,5 +1,5 @@
 import Header_Intro from './Header_intro/Header_intro';
-import Main_image from './Intro_page_icons_and_image/Main_image.png';
+// import Main_image from './Intro_page_icons_and_image/Main_image.png';
 import { Link } from 'react-router-dom';
 import './Intro_page.scss';
 import './Intro_page_media.scss';
@@ -22,7 +22,7 @@ export default function Intro_page({ lang,setLang }) {
 
                 <div className='intro_image_title'>
                     <h1 className='intro_page_title'> Spy game </h1>
-                    <div className='intro_page_image_main'><img src={Main_image} alt="Main_image" className='intro_page_image_main' /></div>
+                    <div className='intro_page_image_main'>{/*<img src={Main_image} alt="Main_image" className='intro_page_image_main' />*/}</div>
                 </div>
 
                 <Link to="/categories" className="intro_page_link"><button className='intro_button'> Play </button></Link>
