b25023a915ecac96b64fc6bfb71c722d328f6452
Vahe <vahehovakimyan574@gmail.com>
2022-08-12 16:04:46 +0400

    ch

--- src/Spy_game/Words_and_spy_page/Words_Spy_page.jsx
+++ src/Spy_game/Words_and_spy_page/Words_Spy_page.jsx
@@ -30,7 +30,7 @@ export default function Words_and_Spy({ participians, ready_array, word_index, s
                     {word_index % 2 === 1 && setIndex(index + 1)}
                     setWord_index(word_index + 1);
                 }}
-            >{((participians * 2) - 1 >= word_index) ? <Link to="/time" className="link"><div className="link_text"> {lang === 0 ? "Ժամանակ" : lang === 1 ? "Время" : "Time"}  </div></Link> : (word_index % 2 === 0) ? (lang === 0 ? "Բացել" : lang === 1 ? "Открыть" : "Open") : (lang === 0 ? "Փակել" : lang === 1 ? "Закрыть" : "Close")} </button>
+            >{((participians * 2) - 1 <= word_index) ? <Link to="/time" className="link"><div className="link_text"> {lang === 0 ? "Ժամանակ" : lang === 1 ? "Время" : "Time"}  </div></Link> : (word_index % 2 === 0) ? (lang === 0 ? "Բացել" : lang === 1 ? "Открыть" : "Open") : (lang === 0 ? "Փակել" : lang === 1 ? "Закрыть" : "Close")} </button>
                 <h1>{word_index}</h1>
         </div>
 
