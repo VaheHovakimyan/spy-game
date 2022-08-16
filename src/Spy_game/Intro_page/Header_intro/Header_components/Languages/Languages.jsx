821f4e83573de52ba6bb61d63aa29ae00f8d4f30
Vahe <vahehovakimyan574@gmail.com>
2022-08-16 15:40:34 +0400

    ch

--- src/Spy_game/Intro_page/Header_intro/Header_components/Languages/Languages.jsx
+++ src/Spy_game/Intro_page/Header_intro/Header_components/Languages/Languages.jsx
@@ -1,5 +1,7 @@
 import Close from '../../../Intro_page_icons_and_image/Close.png';
-import Armenian_flag from '../../../Intro_page_icons_and_image/Armenian_flag.png';
+import Armenian_language from '../../../Intro_page_icons_and_image/Armenian_language.png';
+import Russian_language from '../../../Intro_page_icons_and_image/Russian_language.png';
+import English_language from '../../../Intro_page_icons_and_image/English_language.png';
 import '../../Header_intro.scss';
 import './Languages.scss';
 
@@ -23,11 +25,11 @@ export default function Languages_comp({ showLanguages,setShowLanguages,lang,set
 
                 <div className='languages_info_div'>
                     <div className='languages_buttons'>
-                        <div onClick={() => { setLang(0) }}> <img src={Armenian_flag} className='language_button' alt="Armenian_language" /> </div>
+                        <div onClick={() => { setLang(0) }}> <img src={Armenian_language} className='language_button' alt="Armenian_language" /> </div>
 
-                        <div className='language_button' onClick={() => { setLang(1) }}> Ruseren </div>
+                        <div onClick={() => { setLang(1) }}> <img src={Russian_language} className='language_button' alt="Russian_language" /> </div>
 
-                        <div className='language_button' onClick={() => { setLang(2) }}> Angleren </div>
+                        <div onClick={() => { setLang(2) }}> <img src={English_language} className='language_button' alt="English_language" /> </div>
                     </div>
                     <div>
                         {lang === 0 ? "Ավարտ" : lang === 1 ? "Конец" : "End"}
